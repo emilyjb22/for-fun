0899da7f14010c3c347990b2e10248becfeb225e
Emily Brereton <86748330+emilyjb22@users.noreply.github.com>
2025-09-17 22:11:46 -0700

    Confirmed file is working, cleared out unnecessary code, created Dashboard and Person classes.

---
+++ paper-people/src/main/java/emily/App.java
@@ -0,0 +1,15 @@
+package emily;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+
+        
+    }
+}
+
+class Dashboard {
+
+}
---
+++ paper-people/src/main/java/emily/Person.java
@@ -0,0 +1,32 @@
+package emily;
+
+public class Person {
+
+     // instance variables
+    String firstName;
+    String lastName;
+    String sex;
+    String hairColor;
+    String eyeColor;
+    String skinColor;
+    String freckles;
+
+    // default constructor. Would eventually need to pull random name from an external list...?
+    Person() {
+        this.firstName = "John";
+        this.lastName = "Doe";
+        this.sex = "XY";
+    }
+    
+
+    Person(String firstName, String lastName, String sex){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{name='" + firstName + " " + lastName + "'}";
+    }
+}
