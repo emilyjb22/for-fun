205a7838aef02fa888ec6bda3bdaf4f1137f6fba
Emily Brereton <86748330+emilyjb22@users.noreply.github.com>
2025-09-17 22:47:25 -0700

    added display menu option to dashboard

--- paper-people/src/main/java/emily/App.java
+++ paper-people/src/main/java/emily/App.java
@@ -1,11 +1,22 @@
 package emily;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
+
+        //print title menu
         System.out.println(new Dashboard().title);
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Press Enter to continue...");
+        input.nextLine();
+
+        System.out.println(new Dashboard().menu);
+
 
         
     }
@@ -13,10 +24,22 @@ public class App {
 
 class Dashboard {
 
-
     String title ="""
                 --------------------------------------
                         Paper People Project
+                --------------------------------------
+                    A game of simple inheritance
+                --------------------------------------
+                """;
+
+    String menu = """
+                --------------------------------------
+                Menu:
+                   1.  Create a New Person (Random)
+                   2.  Create a New Person (Custom)
+                   3.  Have a Baby
+                   4.  View Your Population
+                   5.  Exit
                 --------------------------------------
                 """;
 
