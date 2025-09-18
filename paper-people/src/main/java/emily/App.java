bad441509350ff8fc670a984f9c1ca9a4b835563
Emily Brereton <86748330+emilyjb22@users.noreply.github.com>
2025-09-17 22:27:50 -0700

    Added dashboard title

--- paper-people/src/main/java/emily/App.java
+++ paper-people/src/main/java/emily/App.java
@@ -5,11 +5,19 @@ package emily;
  */
 public class App {
     public static void main(String[] args) {
+        System.out.println(new Dashboard().title);
 
         
     }
 }
 
 class Dashboard {
+
+
+    String title ="""
+                --------------------------------------
+                        Paper People Project
+                --------------------------------------
+                """;
 
 }
